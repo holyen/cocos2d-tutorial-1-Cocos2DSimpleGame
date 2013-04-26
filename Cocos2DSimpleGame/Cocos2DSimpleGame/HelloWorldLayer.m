6f72f04c39fc62eb2926596f823aaf95836b1e2c
HolyenZou <holyenzou@gmail.com>
2013-04-26 10:42:26 +0800

    add monster.

--- Cocos2DSimpleGame/Cocos2DSimpleGame/HelloWorldLayer.m
+++ Cocos2DSimpleGame/Cocos2DSimpleGame/HelloWorldLayer.m
@@ -42,8 +42,60 @@
         CCSprite *player = [CCSprite spriteWithFile:@"player.png"];
         player.position = ccp(player.contentSize.width / 2, winSize.height / 2);
         [self addChild:player];
+        [self schedule:@selector(gameLogic:) interval:1.5];
     }
     return self;
+}
+
+- (void)gameLogic:(ccTime)dt
+{
+    [self addMonster];
+}
+
+- (void)addMonster
+{
+    CCSprite *monster = [CCSprite spriteWithFile:@"monster.png"];
+    
+    //Determine where to spawn the monster along the Y axis.
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    int minY = monster.contentSize.height / 2;
+    int maxY = winSize.height - monster.contentSize.height / 2;
+    int rangeY = maxY - minY;
+    
+    /**   
+     arc4random() 比较精确不需要生成随即种子
+     
+     使用方法 ：
+     
+     通过arc4random() 获取0到x-1之间的整数的代码如下：
+     
+     int value = arc4random() % x;
+     
+     
+     获取1到x之间的整数的代码如下:
+     
+     int value = (arc4random() % x) + 1;  
+     */
+    int actualY = (arc4random() % rangeY) + minY; // 随机范围:minY - rangeY
+    
+    /** Create the monster slightly off-screen along the right edge, and along a random position along the Y axis as calculated above:actualY. */
+    monster.position = ccp(winSize.width + monster.contentSize.width / 2, actualY);
+    [self addChild:monster];
+    
+    //Determine speed of the monster.
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;// 2 - 4 ?
+    
+    //Create the actions.
+    CCMoveTo *actionMove = [CCMoveTo actionWithDuration:actualDuration position:ccp(-monster.contentSize.width / 2, actualY)];;
+    CCCallBlockN *actionMoveDone = [CCCallBlockN actionWithBlock:^(CCNode *node) {
+        [node removeFromParentAndCleanup:YES];
+    }];/** you’re going to set up this action to run after the monster goes offscreen to the left – and you’ll remove the monster from the layer when this occurs for not leak memory. */
+    
+    /** The CCSequence action allows us to chain together a sequence of actions that are performed in order, one at a time. This way, you can have the CCMoveTo action perform first, and once it is complete perform the CCCallBlockN action. */
+    [monster runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
 }
 
 // on "dealloc" you need to release all your retained objects
